c4505fee0e57672274d11c5940261a0d1e713fb9
ameni <ameni.bannour.inf@gmail.com>
2024-08-06 05:26:31 +0100

    preparing the Btstp template + preparing the first table

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from app.models import GeneralInfo
 # Register your models here.
+
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+    pass
